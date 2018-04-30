286980e5283ae73732c0f9906d5d64aa27d2e93c
SmellyCat <smellycat2014@foxmail.com>
2018-05-01 00:35:34 +0800

    更改 url路径

--- helper_union_kakou.py
+++ helper_union_kakou.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 import json
 
 import requests
@@ -6,18 +6,20 @@ import requests
 
 class UnionKakou(object):
     def __init__(self, **kwargs):
-        self.host = kwargs['host']
-        self.port = kwargs['port']
-        self.headers = {'content-type': 'application/json'}
-
+        self.base_path = 'http://{0}:{1}{2}'.format(
+            kwargs['host'], kwargs['port'], '/union-data')
+        self.headers = {
+            'content-type': 'application/json',
+            'apikey': kwargs['apikey']
+        }
         self.status = False
 
 
     def get_alarm_maxid(self):
         """获取过车报警最大ID值"""
-        url = 'http://{0}:{1}/alarm/maxid'.format(self.host, self.port)
+        url = '{0}/alarm/maxid'.format(self.base_path)
         try:
-            r = requests.get(url)
+            r = requests.get(url, headers=self.headers)
             if r.status_code == 200:
                 return json.loads(r.text)
             else:
@@ -30,10 +32,9 @@ class UnionKakou(object):
 
     def get_alarm_by_id(self, _id):
         """根据ID范围获取过车报警信息"""
-        url = 'http://{0}:{1}/alarm/{2}'.format(
-            self.host, self.port, _id)
+        url = '{0}/alarm/{1}'.format(self.base_path, _id)
         try:
-            r = requests.get(url)
+            r = requests.get(url, headers=self.headers)
             if r.status_code == 200 or r.status_code == 404:
                 return json.loads(r.text)
             else:
@@ -45,12 +46,12 @@ class UnionKakou(object):
             raise
 
 
-    def get_traffic_crossing_info(self, dict):
+    def get_traffic_crossing_info(self, param):
         """根据ID获取卡口地点信息"""
-        url = 'http://{0}:{1}/traffic_crossing_info?q={2}'.format(
-            self.host, self.port, json.dumps(dict))
+        url = '{0}/traffic_crossing_info?q={1}'.format(
+            self.base_path, json.dumps(param))
         try:
-            r = requests.get(url)
+            r = requests.get(url, headers=self.headers)
             if r.status_code == 200:
                 return json.loads(r.text)
             else:
@@ -64,10 +65,10 @@ class UnionKakou(object):
 
     def get_traffic_crossing_info_by_id(self, crossing_id):
         """根据ID获取卡口地点信息"""
-        url = 'http://{0}:{1}/traffic_crossing_info/{2}'.format(
-            self.host, self.port, crossing_id)
+        url = '{0}/traffic_crossing_info/{1}'.format(
+            self.base_path, crossing_id)
         try:
-            r = requests.get(url)
+            r = requests.get(url, headers=self.headers)
             if r.status_code == 200 or r.status_code == 404:
                 return json.loads(r.text)
             else:
@@ -79,12 +80,12 @@ class UnionKakou(object):
             raise
 
 
-    def get_control_unit(self, dict):
+    def get_control_unit(self, param):
         """根据ID获取控制单元信息"""
-        url = 'http://{0}:{1}/control_unit?q={2}'.format(
-            self.host, self.port, json.dumps(dict))
+        url = '{0}/control_unit?q={1}'.format(
+            self.base_path, json.dumps(param))
         try:
-            r = requests.get(url)
+            r = requests.get(url, headers=self.headers)
             if r.status_code == 200:
                 return json.loads(r.text)
             else:
@@ -98,10 +99,10 @@ class UnionKakou(object):
 
     def get_control_unit_by_id(self, control_unit_id):
         """根据ID获取控制单元信息"""
-        url = 'http://{0}:{1}/control_unit/{2}'.format(
-            self.host, self.port, control_unit_id)
+        url = '{0}/control_unit/{1}'.format(
+            self.base_path, control_unit_id)
         try:
-            r = requests.get(url)
+            r = requests.get(url, headers=self.headers)
             if r.status_code == 200 or r.status_code == 404:
                 return json.loads(r.text)
             else:
@@ -113,12 +114,12 @@ class UnionKakou(object):
             raise
 
 
-    def get_traffic_sysdict(self, dict):
-        """根据ID获取控制单元信息"""
-        url = 'http://{0}:{1}/traffic_sysdict?q={2}'.format(
-            self.host, self.port, json.dumps(dict))
+    def get_traffic_sysdict(self, param):
+        """获取配置信息"""
+        url = '{0}/traffic_sysdict?q={1}'.format(
+            self.base_path, json.dumps(param))
         try:
-            r = requests.get(url)
+            r = requests.get(url, headers=self.headers)
             if r.status_code == 200 or r.status_code == 404:
                 return json.loads(r.text)
             else:
@@ -131,10 +132,11 @@ class UnionKakou(object):
 
 
     def get_stat(self, param):
-        """根据时间获取车流量"""
-        url = 'http://{0}:{1}/stat'.format(self.host, self.port)
+        """根据条件流量统计"""
+        url = '{0}/stat'.format(self.base_path)
         try:
-            r = requests.get(url, params=param)
+            r = requests.get(url, headers=self.headers, params=param)
+            #print(r.url)
             if r.status_code == 200:
                 return json.loads(r.text)
             else:
