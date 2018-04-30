ebd77ea8d4cbf9eff152f729a8febb674ea0fc45
SmellyCat <smellycat2014@foxmail.com>
2018-05-01 00:35:58 +0800

    添加 header里apikey

--- helper_sms.py
+++ helper_sms.py
@@ -9,13 +9,16 @@ class SMS(object):
         self.host = kwargs['host']
         self.port = kwargs['port']
         self.user_name = kwargs['user_name']
-        self.headers = {'content-type': 'application/json'}
-
+        self.headers = {
+            'content-type': 'application/json',
+            'apikey': kwargs['apikey']
+        }
+        self.path = '/sms'
         self.status = False
 
     def sms_send(self, content, mobiles):
         """发送短信"""
-        url = 'http://{0}:{1}/sms'.format(self.host, self.port)
+        url = 'http://{0}:{1}{2}/sms'.format(self.host, self.port, self.path)
         data = {'content': content, 'mobiles': mobiles, 'user_name': self.user_name}
         try:
             r = requests.post(url, headers=self.headers, data=json.dumps(data))
